1e9696711b84b9c164df5c68c127011d290c1389
sashi0034 <satoshi3104.nishi@gmail.com>
2023-02-24 12:41:00 +0900

    グラフィック調整など大量

---
+++ Assets/MissileReflex/Src/Utils/EditorMeshUtil.cs
@@ -0,0 +1,18 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace MissileReflex.Src.Utils
+{
+    public class MeshEditorUtil : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        [Button]
+        public void SaveMesh()
+        {
+            var mesh = GetComponent<MeshFilter>();
+            UnityEditor.AssetDatabase.CreateAsset(mesh.sharedMesh, $"Assets/{nameof(MissileReflex)}/StaticResources/{gameObject.name}.asset");
+            UnityEditor.AssetDatabase.SaveAssets();
+        }
+#endif
+    }
+}
---
+++ Assets/MissileReflex/Src/Utils/EditorSnapUtil.cs
@@ -0,0 +1,37 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace MissileReflex.Src.Utils
+{
+    public class EditorSnapUtil : MonoBehaviour
+    {
+        [Button]
+        public void SnapPosXZEachChildren(float snapping = 0.5f)
+        {
+            foreach (var child in transform.GetChildren())
+            {
+                var currPos = child.transform.position;
+                var newPos = new Vector3(
+                    snapValue(currPos.x, snapping),
+                    currPos.y,
+                    snapValue(currPos.z, snapping));
+                child.transform.position = newPos;
+            }
+        }
+
+        private static float snapValue(float value, float snapping)
+        {
+            return Mathf.Floor((value + snapping / 2) / snapping) * snapping;
+        }
+
+        [Button]
+        public void RandomRotYEachChildren(float snapping = 15)
+        {
+            foreach (var child in transform.GetChildren())
+            {
+                var rotY = snapping * Random.Range(0, (int)(360 / snapping));
+                child.transform.rotation = Quaternion.Euler(new Vector3(0, rotY, 0));
+            }
+        }
+    }
+}
